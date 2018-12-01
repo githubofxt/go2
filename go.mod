afb9cc0d08f3c7c6efc26aa30f94968bcf4b290d
githubofxt <xt@163.com>
2018-12-01 16:36:53 +0800

    go2 module init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/githubofxt/go2
+
+require github.com/githubofxt/go1 v0.0.0-20181201075751-6c78c3b1117a
+
+replace github.com/githubofxt/go1 => github.com/githubofxt/go1 v0.0.0-20181201075751-6c78c3b1117a
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+ "fmt"
+ "github.com/githubofxt/go1"
+ "github.com/githubofxt/go1/net"
+ "github.com/githubofxt/go1/json"
+)
+
+func main(){
+  fmt.Println(" -----------------------")
+  fmt.Println("go2 module  is running")
+  g.Go1()
+  fmt.Println(net.Net("NNNEEETTT"))
+  fmt.Println(json.Json("JJJSSSOOONNN"))
+  fmt.Println(" -----------------------")
+}
